861c0f10593c36d619833852a248b66f99d4bd15
Narcro <ben.blot@yahoo.fr>
2024-04-20 13:04:00 +0200

    Update chathub

--- ChatService/Entities/Message.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChatService.Entities;
-
-public class Message
-{
-    public int Id { get; set; }
-    public string Content { get; set; }
-    public string Sender { get; set; }
-    public DateTime Timestamp { get; set; }
-}
